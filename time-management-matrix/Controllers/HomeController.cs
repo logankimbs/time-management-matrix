cc1c14a166eca1405c1a383d5ef5772329066b01
kfromm <kyliefromm@gmail.com>
2022-02-09 11:21:36 -0700

    made controllers, fixed models, and also views

--- time-management-matrix/Controllers/HomeController.cs
+++ time-management-matrix/Controllers/HomeController.cs
@@ -5,24 +5,48 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MovieCollection.Models;
 using time_management_matrix.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace time_management_matrix.Controllers
 {
     public class HomeController : Controller
     {
+        private TaskFormContext FormContext { get; set; }
+
+        public HomeController(TaskFormContext someName)
+        {
+            FormContext = someName;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpGet]
-        public IActionResult Tasks()
+        public IActionResult TaskForm()
         {
+            ViewBag.Categories = FormContext.Categories.ToList();
             return View();
         }
 
-        [HttpGet]
+        [HttpPost]
+        public IActionResult TaskForm(TaskForm ar)
+        {
+            if (ModelState.IsValid)
+            {
+                FormContext.Add(ar);
+                FormContext.SaveChanges();
+                return View();
+            }
+
+            else
+            {
+                ViewBag.Categories = FormContext.Categories.ToList();
+                return View();
+            }
+        }
         public IActionResult CreateTask()
         {
             return View();
